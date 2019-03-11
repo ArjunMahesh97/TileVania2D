1e5ad4a51bf60758711f371be9dcb956ed1af8f2
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-03-12 02:05:33 +0530

    Charecter jump

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float runSpead = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Run();
+        FlipSprite();
+        Jump();
+	}
+
+    void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow*runSpead,myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+        bool playerHasSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasSpeed);
+    }
+
+    void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump")){
+                Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
+                myRigidbody.velocity = jumpVelocity;
+            }
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+}
