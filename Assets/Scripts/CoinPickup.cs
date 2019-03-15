161a1eebf096a117949b51c9be26b58a0cad355a
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-03-15 17:07:58 +0530

    Score and Lives Display

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour {
 
+    [SerializeField] AudioClip coinPickupSFX;
+    [SerializeField] int CoinPoints = 100;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        FindObjectOfType<GameSession>().UpdateScore(CoinPoints);
+        AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 
